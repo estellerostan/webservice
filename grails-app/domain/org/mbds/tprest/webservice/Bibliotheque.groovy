d9bc7fbef3b53d881da20dc698ba6d7f77ed5667
Estelle Rostan <estelle.rostan@etu.unice.fr>
2017-10-09 14:14:19 +0200

    Ajoute les domaines Bibliotheque et Livre
    Selon le modèle de donnée voulue, sans la liaison de composition entre les deux.

---
+++ grails-app/domain/org/mbds/tprest/webservice/Bibliotheque.groovy
@@ -0,0 +1,11 @@
+package org.mbds.tprest.webservice
+
+class Bibliotheque {
+
+    String nom
+    String adresse
+    int anneeConstruction
+
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/org/mbds/tprest/webservice/Livre.groovy
@@ -0,0 +1,12 @@
+package org.mbds.tprest.webservice
+
+class Livre {
+
+    String nom
+    Date dateParution
+    String ISBN
+    String auteur
+
+    static constraints = {
+    }
+}
