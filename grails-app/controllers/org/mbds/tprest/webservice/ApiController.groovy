3e63bc4458daaa6e3f85a4e846970ab13f914c76
Estelle Rostan <estelle.rostan@etu.unice.fr>
2017-10-15 23:59:23 +0200

    Renvoie une unique ressource demandée en GET
    Lue par son id, si la ressource associée à l'id n'existe pas, alors renvoie un code 404.

--- grails-app/controllers/org/mbds/tprest/webservice/ApiController.groovy
+++ grails-app/controllers/org/mbds/tprest/webservice/ApiController.groovy
@@ -1,6 +1,8 @@
 package org.mbds.tprest.webservice
 
 import grails.converters.JSON
+import groovy.json.JsonOutput
+import groovy.json.StreamingJsonBuilder
 
 class ApiController {
 
@@ -12,7 +14,31 @@ class ApiController {
 
         switch (request.getMethod()) {
             case "GET":
-                response.status = 200
+
+                // représentation de ma ressource par id
+                if (params.id != null) {
+
+                    Livre leLivre = ((Livre.findById( params.id) == null) ? null : Livre.findById(params.id))
+
+                    if (leLivre == null)
+                        response.status = 404
+                    else {
+                        StringWriter writer = new StringWriter()
+                        StreamingJsonBuilder builder = new StreamingJsonBuilder(writer)
+                        builder.livre {
+                            nom leLivre.nom
+                            dateparution leLivre.dateParution
+                            isbn leLivre.ISBN
+                            auteur leLivre.auteur
+                        }
+                        String json = JsonOutput.prettyPrint(writer.toString())
+
+                        response.status = 200
+                        response.outputStream << json
+                    }
+                } else { // représentation de ma collection
+                    response.status = 404
+                }
                 break
 
             case "POST":
